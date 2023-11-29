b2474d80d0d8a437c4a355e14b5021edc632d66b
xTirian <tirianx@yahoo.com>
2023-11-28 23:20:16 -0300

    feat: add main view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
+"use client";
 import Image from "next/image";
-import styles from "./page.module.scss";
+import "./style.scss";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [widthFlag, setWidthFlag] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleResize() {
+      setWidthFlag(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  console.log(typeof widthFlag);
+
   return (
-    <main className={styles.main}>
-      main
+    <main className="main_container">
+      <section className="main_sect">
+        <h1 className="main_title">Wait ! your order in progress.</h1>
+        
+        <h2 className="main_subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing </h2>
+      </section>
     </main>
   );
+}
+
+function getWindowSize() {
+  const { innerWidth } = window;
+  return innerWidth;
 }
