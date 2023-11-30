4bb6d24657a2161cdb738243c03ac3e1f61f879e
xTirian <tirianx@yahoo.com>
2023-11-29 23:44:44 -0300

    feat: finished mobile version

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <div>
+        <p>Copyright (C) 2023</p>
+
+        <span className="cardPattern1"></span>
+
+        <p>clarifionsupport@clarifion.com</p>
+      </div>
+      <div>
+        <img src="/images/icons/lock.png" alt="lockicon" />
+        <p className="secureTextFooter">Secure 256-bit SSL encryption.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
