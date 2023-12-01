3ed1037ba435202f4bdadcf78213bef2e7085514
xTirian <tirianx@yahoo.com>
2023-12-01 01:49:21 -0300

    fix stule bug button

--- src/components/button-grid/index.tsx
+++ src/components/button-grid/index.tsx
@@ -1,45 +1,55 @@
-'use client';
-import React from 'react'
-import './style.scss'
+"use client";
+import React from "react";
+import "./style.scss";
 
-const ButtonGrid = ({widthWindow, changeView}: number| any) => {
+const ButtonGrid = ({ widthWindow, changeView }: number | any) => {
+  
+  console.log(widthWindow)
 
-  //TODO pass the info of display to parent by clicking in button
   return (
-    <div className='buttons_container'>
-      <button className='confirmButton' onClick={() => changeView("")}>
-        YES - CLAIM MY DISCOUNT <img src="/images/icons/arrow.png" alt="arrow" />
+    <div className="buttons_container">
+      <button className="confirmButton" onClick={() => changeView("")}>
+        YES - CLAIM MY DISCOUNT{" "}
+        <img src="/images/icons/arrow.png" alt="arrow" />
       </button>
 
-      <div className='secureInfo_card'>
+      <div className="secureInfo_card">
         <div>
           <p>Free shipping</p>
 
-          <span className='cardPattern1'>
+          <span className="cardPattern1"></span>
 
-          </span>
+          <p>Secure 256-bit SSL encryption</p>
+        </div>
+        {widthWindow < 760 ? (
+          <span className="cardPattern2"></span>
+        ) : (
+          <span className="cardPattern1"></span>
+        )}
 
-          <p>
-          Secure 256-bit SSL encryption
-          </p>
-          </div>
-          
-          <span className={widthWindow < 760? "cardPattern2": "cardPattern1"}></span>
-          <img src="/images/CardsFlag.png" alt="cardsflag" className='cardsFlagImg' />
+        <img
+          src="/images/CardsFlag.png"
+          alt="cardsflag"
+          className="cardsFlagImg"
+        />
       </div>
 
-      <button className='returnButton' onClick={() => changeView("return")}>No thanks, I don’t want this.</button>
+      <button className="returnButton" onClick={() => changeView("return")}>
+        No thanks, I don’t want this.
+      </button>
 
-      <div className='satisfactionGrid'>
+      <div className="satisfactionGrid">
         <img src="/images/desktop/satisfaction.png" alt="" />
         <div>
           <p>
-          If you are not completely thrilled with your Clarifion - We have a <b>30 day satisfaction guarantee</b>. Please refer to our return policy at the bottom of the page for more details. Happy Shopping!
+            If you are not completely thrilled with your Clarifion - We have a{" "}
+            <b>30 day satisfaction guarantee</b>. Please refer to our return
+            policy at the bottom of the page for more details. Happy Shopping!
           </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonGrid
+export default ButtonGrid;
